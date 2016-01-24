c3658ba4fbfe99590fdf76286e0f1b804d882187
AdrianHsu <adrianhsu1995@gmail.com>
2016-01-24 15:45:25 +0800

    first commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerController: NetworkBehaviour 
+{
+	public UnityStandardAssets.Characters.FirstPerson.FirstPersonController fpsController;
+	public Camera fpsCamera;
+	public AudioListener audioListener;
+	//public ShootingScript shootingScript;
+
+	public override void OnStartLocalPlayer()
+	{
+		//多人連線所有的Player都有相同的控制項目，所以我們把這些項目都先關閉，只有在一開始控制自己的角色時，以下的控制才打開已防止操作相互干擾。
+		fpsController.enabled = true;
+		fpsCamera.enabled = true;
+		audioListener.enabled = true;
+		//shootingScript.enabled = true;
+
+		gameObject.name = "ME";
+		base.OnStartLocalPlayer ();
+	}
+
+}
+
---
+++ Assets/Scripts/Tank/TankHP.cs
@@ -0,0 +1,36 @@
+﻿//This script tracks the health of the tower and game status
+
+using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class TankHP : NetworkBehaviour
+{
+	AudioSource damageAudio;		//Audio feedback of hit
+
+    void Awake()
+	{
+		//Set current lives and get audio component reference
+     	damageAudio = GetComponent<AudioSource>();
+    }
+
+	void OnTriggerEnter(Collider other)
+	{
+		//Make sure we can only be hit by enemies and only if tower is alive
+		//如果坦克碰到標籤是Bullet的物件，就播放音效並通知Server毀滅此坦克
+		if (other.tag != "Bullet")
+			return;
+		
+		damageAudio.Stop ();
+		CmdDestoryTank ();
+		damageAudio.Play ();
+
+	}
+
+	//告訴Server坦克死了
+	[Command]
+	void CmdDestoryTank()
+	{
+		NetworkServer.Destroy(gameObject);
+	}
+}
